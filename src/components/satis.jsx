d6179e1374b00d8c5ab8b719093fb75cc21d9e92
Vodafonefaturaodeme <caspersscasper0@gmail.com>
2024-03-16 08:26:49 +0300

    değişiklik
    navbardaki linkler düzeltildi tek sayfalık düzen oluşturuldu /tos dizini eklendi

---
+++ src/components/satis.jsx
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import  './satis.scss';
+import laptop from '../assets/laptop.jpg'
+const satis = () => {
+    
+
+
+    return(
+        
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body  className="component-LayoutLoginRegister">
+
+        <div className="terms-box">
+            <div className="terms-text">
+                <h2>
+
+                 Satış sözleşmesi
+                </h2>
+                <p>Son güncelleme 14.03.2024</p>
+                <p>Selamlar ,</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nostrum alias in praesentium autem ducimus soluta inventore quia adipisci amet laborum fuga, earum aperiam sequi quam ratione ea. Cum dolorum illum eos vel rerum corrupti eligendi adipisci, blanditiis porro esse harum voluptatem incidunt nobis quia ab voluptate eum doloremque nemo!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores temporibus sunt dicta, id cumque, rerum, quibusdam eum minus magnam quasi dolorum nemo. Cumque doloribus labore beatae esse facere necessitatibus adipisci cum, tempore similique. Reiciendis rerum architecto consequuntur molestiae veniam aspernatur iste voluptatem commodi, aliquam voluptates esse aperiam modi, libero obcaecati!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi blanditiis omnis quae fugiat aut perspiciatis, dolore eos veritatis et tempora quos perferendis, ratione nostrum qui. Voluptatum nobis, dignissimos eos perferendis repellat enim sapiente nam ab amet numquam quos molestiae velit vel praesentium autem commodi eaque quia nulla facere. Facere, error!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quae quo vitae ducimus tempore cumque mollitia iusto aliquam commodi accusamus unde autem odio eaque accusantium perspiciatis id, aperiam libero tenetur. Ducimus ipsum, fuga voluptatibus adipisci ratione architecto corrupti a id, quo animi maxime saepe pariatur sequi iste deserunt perferendis porro.</p>
+
+
+            </div> 
+            <h3>Kabul ediyorum <span>sözleşme</span> ve Gizlilik politikası unuttum</h3>
+            <div className="buttons">
+                <button className="btn red-btn">Kabul et</button>
+                <button className="btn gray-btn"> Reddet</button>
+
+            </div>
+
+        </div>
+            
+        </body>
+        </html>
+        
+        
+    )
+}
+
+export default satis;
